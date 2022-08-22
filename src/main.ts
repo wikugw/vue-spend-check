ff7785ce22c2f193fcb140a9ff9f4d0ce9e07be6
Wiku Galindra Wardhana <wiku.gwardhana@bukitmakmur.com>
2022-08-22 19:42:48 +0700

    setup repo, install element plus n tailind

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import './index.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(ElementPlus).mount("#app");
