6d99a604a60bad027f0fb7e827c64b5d4dc82882
Wiku Galindra Wardhana <wiku.gwardhana@bukitmakmur.com>
2022-08-28 16:23:16 +0700

    add validation

--- src/views/spend-form/Index.vue
+++ src/views/spend-form/Index.vue
@@ -1,39 +1,96 @@
 <template>
   <div class="grid mx-auto p-5 bg-white rounded-lg w-3/4">
-    <form>
+    <el-form
+      ref="ruleFormRef"
+      :model="form"
+      :rules="rules"
+      status-icon
+      label-position="top"
+      scroll-to-error
+    >
         <div class="grid gap-6 mb-6">
-            <div>
-                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nama Pengeluaran</label>
-                <el-input v-model="form.name" size="large" placeholder="Please input" clearable />
-            </div>
-            <div>
-                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Jenis Pengeluaran</label>
-                <el-select v-model="form.type" placeholder="Select" size="large" class="w-full">
-                    <el-option v-for="item in spendingTypes" :key="item" :label="item" :value="item" />
-                </el-select>
-            </div>
-            <div>
-                <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Total Pengeluaran</label>
-                <el-input @keypress="onlyNumber" v-model="form.amount" size="large" placeholder="Please input" clearable />
-            </div>  
+          <div>
+            <el-form-item label="Nama Pengeluaran" prop="name">
+              <el-input v-model="form.name" size="large" placeholder="Please input" clearable />
+            </el-form-item>
+          </div>
+          <div>
+            <el-form-item label="Jenis Pengeluaran" prop="type">
+              <el-select v-model="form.type" placeholder="Select" size="large" class="w-full" clearable>
+                <el-option v-for="item in spendingTypes" :key="item" :label="item" :value="item" />
+              </el-select>
+            </el-form-item>
+          </div>
+          <div>
+            <el-form-item label="Total Pengeluaran" prop="amount">
+              <el-input @keypress="onlyNumber" v-model="form.amount" size="large" placeholder="Please input" clearable />
+            </el-form-item>
+          </div>  
         </div>
-        <button @click.prevent="handlePostSpending()" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-    </form>
+        <button @click.prevent="resetForm(ruleFormRef)" class="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-1">Reset</button>
+        <button @click.prevent="submitForm(ruleFormRef)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-1">Submit</button>
+    </el-form>
   </div>
 </template>
 
 <script setup lang="ts">
 import useSpendFormStore from '@/store/spending/form';
-import { computed } from 'vue';
+import { FormInstance, FormRules } from 'element-plus';
+import { reactive, computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const formStore = useSpendFormStore()
 const spendingTypes = formStore.spendingTypes
 const router = useRouter()
+const ruleFormRef = ref<FormInstance>()
+
+  const rules = reactive<FormRules>({
+  name: [
+    { required: true, message: 'Nama Pengeluaran wajib diisi', trigger: 'blur' },
+  ],
+  type: [
+    {
+      required: true,
+      message: 'Jenis Pengeluaran wajib diisi',
+      trigger: 'change',
+    },
+  ],
+  amount: [
+    {
+      required: true,
+      message: 'Total Pengeluaran harus diisi',
+      trigger: 'blur',
+    },
+  ],
+  date1: [
+    {
+      type: 'date',
+      required: true,
+      message: 'Please pick a date',
+      trigger: 'change',
+    },
+  ],
+})
 
 const form = computed(() => {
   return formStore.form
 })
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      handlePostSpending()
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
 
 const handlePostSpending = () => {
   formStore.postSpending().then(() => {
